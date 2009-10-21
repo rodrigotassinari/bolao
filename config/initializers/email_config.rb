6e9e58b5f64e221c10c6fc13deafaba2d372a34c
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-10-21 21:45:21 -0200

    Importação inicial.

---
+++ config/initializers/email_config.rb
@@ -0,0 +1,17 @@
+ActionMailer::Base.default_url_options[:host] = Settings.email.domain
+
+if Rails.env.production? || Rails.env.staging?
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.default_charset = 'utf-8'
+
+  ActionMailer::Base.smtp_settings = {
+    :address => Settings.email.server,
+    :domain => Settings.email.domain,
+    :port => Settings.email.port,
+    :authentication => :login,
+    :user_name => Settings.email.login,
+    :password => Settings.email.password
+  }
+end
