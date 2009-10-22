bb2de04cbc94f9d18c50b7040a74c3e069b8176e
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-10-22 00:01:56 -0200

    usuários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,51 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  helper_method :current_user_session, :current_user, :logged_in?
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  filter_parameter_logging :password, :password_confirmation
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  private
+
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def require_user
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page" #FIXME traduzir
+        redirect_to login_url
+        return false
+      end
+    end
+
+    def require_no_user
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page" #FIXME traduzir
+        redirect_to root_url
+        return false
+      end
+    end
+
+    def store_location
+      session[:return_to] = request.request_uri
+    end
+
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,15 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  map.resource :user_session
+  map.resources :users
+
+  map.login '/login', :controller => "user_sessions", :action => "new"
+  map.logout '/logout', :controller => "user_sessions", :action => "destroy"
+  map.signup '/signup', :controller => "users", :action => "new"
+
+  map.root :controller => "pages", :action => "index"
+
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
