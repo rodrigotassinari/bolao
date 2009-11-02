4d2addbe11335dbd8902f572708a9f5a05fadc4d
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-11-01 22:02:38 -0200

    validações de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
+
+  DEFAULT_PREFERENCES = {
+    :receive_newsletter => true
+  }
+
   acts_as_authentic
-#  acts_as_authentic do |c|
-#    c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
-#  end # block optional
 
+  serialize :preferences, Hash
 
+  validates_presence_of :email, :login, :preferences
+
+  validates_uniqueness_of :email, :login
+
+  attr_accessible :login, :email, :name, :password, :password_confirmation
+
+  # default attribute values
+  def after_initialize
+    if new_record?
+      # set defaults only if the attribute is not set already
+      self.preferences = DEFAULT_PREFERENCES if self.preferences.nil?
+    end
+  end
 
 end
