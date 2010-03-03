b08afa536436985a6ab889613a18d79d696b3d74
Rodrigo Tassinari de Oliveira <roliveira@riopro.com.br>
2010-03-03 15:51:24 -0300

    Remove as colunas específicas do authligic de User

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091102230752) do
+ActiveRecord::Schema.define(:version => 20100303184213) do
 
   create_table "bets", :force => true do |t|
     t.integer  "user_id"
@@ -73,31 +73,12 @@ ActiveRecord::Schema.define(:version => 20091102230752) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "login",                                  :null => false
-    t.string   "email",                                  :null => false
-    t.string   "crypted_password",                       :null => false
-    t.string   "password_salt",                          :null => false
-    t.string   "persistence_token",                      :null => false
-    t.string   "single_access_token",                    :null => false
-    t.string   "perishable_token",                       :null => false
-    t.integer  "login_count",         :default => 0,     :null => false
-    t.integer  "failed_login_count",  :default => 0,     :null => false
-    t.datetime "last_request_at"
-    t.datetime "current_login_at"
-    t.datetime "last_login_at"
-    t.string   "current_login_ip"
-    t.string   "last_login_ip"
-    t.boolean  "admin",               :default => false
+    t.boolean  "admin",        :default => false
     t.text     "preferences"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "paid_at"
-    t.integer  "points_cache",        :default => 0
+    t.integer  "points_cache", :default => 0
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["last_request_at"], :name => "index_users_on_last_request_at"
-  add_index "users", ["login"], :name => "index_users_on_login"
-  add_index "users", ["persistence_token"], :name => "index_users_on_persistence_token"
 
 end
