b9d354f47378b26d0ed1a7e5cdb37cf4a21f6280
Rodrigo Tassinari de Oliveira <roliveira@riopro.com.br>
2010-03-09 10:26:11 -0300

    Apaga tudo pra fazer de novo

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
-class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  filter_parameter_logging :fb_sig_friends
-end
-
--- config/initializers/global_requires.rb
+++
@@ -1,1 +0,0 @@
-require 'custom_form_helpers'
