306afc2104d83bffe09199482791ef905b56c187
Rodrigo Tassinari de Oliveira <roliveira@riopro.com.br>
2010-03-09 10:57:41 -0300

    gem config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,7 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+# para devise
+config.action_mailer.default_url_options = { :host => 'bolao.pittlandia.net' }
+
