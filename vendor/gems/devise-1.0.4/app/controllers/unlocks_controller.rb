41ca442c0c5c88e4ca075ece65855e234ddc3e50
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-03-10 10:14:05 -0300

    vendorizando gems

---
+++ vendor/gems/devise-1.0.4/app/controllers/unlocks_controller.rb
@@ -0,0 +1,33 @@
+class UnlocksController < ApplicationController
+  include Devise::Controllers::InternalHelpers
+
+  # GET /resource/unlock/new
+  def new
+    build_resource
+    render_with_scope :new
+  end
+
+  # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(params[resource_name])
+
+    if resource.errors.empty?
+      set_flash_message :notice, :send_instructions
+      redirect_to new_session_path(resource_name)
+    else
+      render_with_scope :new
+    end
+  end
+
+  # GET /resource/unlock?unlock_token=abcdef
+  def show
+    self.resource = resource_class.unlock!(:unlock_token => params[:unlock_token])
+
+    if resource.errors.empty?
+      set_flash_message :notice, :unlocked
+      sign_in_and_redirect(resource_name, resource)
+    else
+      render_with_scope :new
+    end
+  end
+end
---
+++ vendor/gems/warden-0.9.6/lib/warden/version.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+module Warden
+  VERSION = "0.9.6".freeze
+end
