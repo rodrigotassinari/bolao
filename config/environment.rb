5974d2b299b7d333fd2c93abe2aceed0071a91b8
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-05-23 17:45:43 -0300

    Atualiza aplicação para Rails 2.3.6

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.6' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,10 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml", :version => "2.2.20"
+  config.gem "haml", :version => "3.0.5"
   config.gem "warden", :version => "0.9.6"
   config.gem "devise", :version => "1.0.4"
-  config.gem "recaptcha", :lib => "recaptcha/rails"
+  config.gem "recaptcha", :lib => "recaptcha/rails", :version => '0.2.3'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
