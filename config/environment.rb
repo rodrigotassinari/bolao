f76f33da40e58e94747a2ee92ca7e543968d3625
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-05-30 20:53:00 -0300

    Atualiza para Rails 2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.6' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml", :version => "3.0.5"
+  config.gem "haml", :version => "3.0.6"
   config.gem "warden", :version => "0.9.6"
   config.gem "devise", :version => "1.0.4"
   config.gem "recaptcha", :lib => "recaptcha/rails", :version => '0.2.3'
