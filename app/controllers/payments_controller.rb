d5b1a9a364b25206ff9952dd24f24fd30cb55b47
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-05-30 21:29:40 -0300

    PagSeguro continua não atualizando User corretamente apenas em produção! Adiciona loggers para debugar... :(

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -30,22 +30,33 @@ class PaymentsController < ApplicationController
   end
   
   # GET /payment/done
+  # Funciona em development, funciona em production na máquina local mas não funfa em produção! PQ???
   def done
     if request.post?
+logger.warn ">>> entrou no post"
       pagseguro_notification do |notification|
+logger.warn ">>> entrou no notification"
         raise unless notification.valid?
+logger.warn ">>> válido"
+logger.warn ">>> dados do post: Referencia = #{notification.params["Referencia"]}, TransacaoID = #{notification.params["TransacaoID"]}, StatusTransacao = #{notification.params["StatusTransacao"]}"
+logger.warn ">>> status parseado do post: #{notification.status}"
         user = User.find_by_payment_code(notification.params["Referencia"]) || User.find_by_email(notification.buyer[:email])
         raise if user.nil?
+logger.warn ">>> achou user: id = #{user.id}, nome = #{user.name}, payment_code = #{user.payment_code}"
+logger.warn ">>> ANTES: payment_transaction_code = #{user.payment_transaction_code}, paid_at = #{user.paid_at}"
         user.update_attribute(:payment_transaction_code, notification.params["TransacaoID"])
         user.update_attribute(:paid_at, nil)
         user.update_attribute(:paid_at, Time.current) if notification.status == :completed
         user.update_attribute(:paid_at, Time.current) if notification.status == :approved
         user.update_attribute(:paid_at, Time.current) if notification.params["StatusTransacao"] == "Completo"
         user.update_attribute(:paid_at, Time.current) if notification.params["StatusTransacao"] == "Aprovado"
+logger.warn ">>> DEPOIS: payment_transaction_code = #{user.payment_transaction_code}, paid_at = #{user.paid_at}"
+        user.reload
+logger.warn ">>> RELOAD: payment_transaction_code = #{user.payment_transaction_code}, paid_at = #{user.paid_at}"
       end
       render :nothing => true
+logger.warn ">>> final, renderiza nada"
     end
   end
   
 end
-
